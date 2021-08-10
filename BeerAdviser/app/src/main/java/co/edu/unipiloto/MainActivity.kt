9ce028b4dfba67bda174ca630839efd930b589be
SebasUni <81190314+SebasUni@users.noreply.github.com>
2021-08-10 14:09:28 -0500

    Update MainActivity.kt

--- BeerAdviser/app/src/main/java/co/edu/unipiloto/MainActivity.kt
+++ BeerAdviser/app/src/main/java/co/edu/unipiloto/MainActivity.kt
@@ -2,11 +2,12 @@ package co.edu.unipiloto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View;
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
 }
